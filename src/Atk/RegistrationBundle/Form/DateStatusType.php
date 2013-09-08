c619501e67ec2dadf55c0926ec8f84c80f1eec13
prsolans <paul@aqdct.com>
2013-09-08 13:09:32 +0000

    DateStatus entity and CRUD creation

---
+++ src/Atk/RegistrationBundle/Form/DateStatusType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Atk\RegistrationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DateStatusType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Atk\RegistrationBundle\Entity\DateStatus'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'atk_registrationbundle_datestatus';
+    }
+}
